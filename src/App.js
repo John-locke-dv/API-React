650da9453fdbdc7cf17549d602ec7615bdeaa11d
John-locke-dv <69838880+John-locke-dv@users.noreply.github.com>
2020-11-22 17:46:10 -0300

    button handler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { component, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import showUser from './showUser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.handlerClick = this.handlerClick.bind(this);
+  }
+  
+  state = {
+    person: false,
+    handlerClick : false,
+  };
+  
+  async componentDidMount() {
+    const url = 'https://randomuser.me/api/';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({person: data.results[0], loading: true})
+  }
+
+  render() {
+    return (
+    <div>
+    {this.state.loading || !this.state.person ? (
+      
+    <div align = 'center'>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      cargando...
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <button onClick = {this.handlerClick}> Consultar API </button>
+      </div>
+    ) : (
+    <div align = 'center'>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div>{this.state.person.name.title}</div>
+      <div>{this.state.person.name.first}</div>
+      <div>{this.state.person.name.last}</div>
+      <img src={this.state.person.picture.large} />
     </div>
-  );
+    )}
+    <br></br>
+    <br></br>
+  </div>
+    )
+    
+  }
+
+  handlerClick() {
+    this.setState({
+      loading : false
+    })
+  }
+
 }
+
 
 export default App;
---
+++ src/showUser.js
@@ -0,0 +1,11 @@
+
+import React, { Component, useEffect, useState } from 'react'
+
+
+class showUser extends Component {
+
+  
+    
+}
+
+export default showUser
